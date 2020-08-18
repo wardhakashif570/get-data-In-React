ce68dd534287bbfbd46484d7dd2165ee1a6a266d
wardhakashif <wardhakashif570@gmail.com>
2020-08-18 04:37:35 -0700

    get data in react

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect( () => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/wardhakashif570/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+    // "https://api.github.com/users/muhammadmohsin/repos"
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     console.log(json);
+    //     setData(json);
+    //   })
+  },[])
+
+  return (
+    <div className="App">
+      <h1>You are seeing all repositories</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
